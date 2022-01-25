158a787f5f0e26153880fb60f669a9e04b250f4d
Evgrafov Ruslan <rusraid@mail.ru>
2022-01-25 14:50:57 +0400

    Initial commit

---
+++ src/main/java/pro/sky/dz24exceptions/Dz24ExceptionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.dz24exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz24ExceptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz24ExceptionsApplication.class, args);
+    }
+
+}
